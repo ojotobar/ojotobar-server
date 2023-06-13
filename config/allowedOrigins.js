e8b22873c8dbe8ccf1b157669accf588ed7ef431
Toba Ojo <tojo@axxess.com>
2023-06-13 13:54:47 +0100

    Added the Client Base Url to the list of allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://127.0.0.1:5500',
-    'http://localhost:3500'
+    'http://localhost:3500',
+    'https://gleaming-tartufo-3d552b.netlify.app'
 ];
 
 module.exports = allowedOrigins;
