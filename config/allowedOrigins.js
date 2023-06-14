bd5b1708f12a0bdee7ffa4833147ff2e12a9b57c
Toba Ojo <tojo@axxess.com>
2023-06-14 02:49:00 +0100

    Added the api remote server url to allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
-    'https://gleaming-tartufo-3d552b.netlify.app'
+    'https://gleaming-tartufo-3d552b.netlify.app',
+    'https://exuberant-lamb-long-underwear.cyclic.app'
 ];
 
 module.exports = allowedOrigins;
