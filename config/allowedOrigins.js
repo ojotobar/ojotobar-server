7576e50d646adb3ce7fc20aea3ce5856e7fd6d9d
Toba Ojo <tojo@axxess.com>
2023-09-12 23:57:37 +0100

    Added new client url to allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
-    'https://gleaming-tartufo-3d552b.netlify.app',
+    'https://ojotobar.netlify.app',
     'https://exuberant-lamb-long-underwear.cyclic.app'
 ];
 
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,8 +2,6 @@ const { logEvents } = require('./logEvents');
 const { format } = require('date-fns');
 
 const errorHandler = (err, req, res, next) => {
-    console.log(res)
-    console.log(err)
     logEvents(`${err.name}: ${err.message}`, `errLog-${format(new Date(), 'yyyyMMdd-HH')}.txt`);
     res.status(500).send(err.message);
 }
